e093b4aad72b52493fc035ed25c30f8bb65e9e04
Alexis Delgado <alexisdelgado@MacBook-Pro-7.local>
2022-11-23 23:05:50 -0800

    adde 404 page routing

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ app.get('/', function (req, res){
     res.send(`hello`)
 })
 
+app.get('*',(req, res) => {
+    res.status(404).send('<h1>404 Page not Found</h1>')
+    
+})
+
 app.listen(process.env.PORT)
