1da9aef914a9feda7a1ece88eb09cbb917f2f9c6
Alexis Delgado <alexisdelgado@MacBook-Pro-7.localdomain>
2022-11-27 19:03:24 -0800

    Added css files, still having an issue with importing Boostrap to customize button

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+//modules and globals
 require('dotenv').config()
-
 const express = require('express')
 const app = express()
 
+//express settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
+app.use(express.static('public'))
 
 //controllers and routes
-
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.render('home')
   })
   
-
 app.get('*',(req, res) => {
     res.status(404).render('error404')
     
 })
+//Listen for connections
 
 app.listen(process.env.PORT)
--- views/default.jsx
+++ views/default.jsx
@@ -4,6 +4,12 @@ function Def (html) {
     return ( <html>
             <head>
                 <title>Rest-Rant</title>
+                <link 
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
+                rel="stylesheet" 
+                integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" 
+                crossOrigin="anonymous"/>
+                <link rel="stylesheet" href='/css/style.css'/>
             </head>
             <body>
                 {html.children}
@@ -14,4 +20,5 @@ function Def (html) {
 }
 
 module.exports = Def
+// UNABLE TO GET BOOTSRAP WORKING, ASK INSTRUCTOR MONDAY NOV 28
 
