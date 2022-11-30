1b413ab25bb0ffe0da282ddcc77a26c1a58e775e
Alexis Delgado <alexisdelgado@MacBook-Pro-7.localdomain>
2022-11-30 05:22:41 -0800

    got form working, the page after the submit isnt coming up.

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
 
+
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+
+
 router.get('/', (req,res) => {
     let places =[
         {
@@ -21,7 +27,12 @@ router.get('/', (req,res) => {
     res.render('places/index', {places})
 })
 
-  
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
+
 module.exports = router
 
 
